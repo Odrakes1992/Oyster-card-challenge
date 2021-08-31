ffc0cce00b0f3a873781b697941c1d04e2620cea
Osneil Drakes <osneil-drakes@hotmail.com>
2021-08-31 23:57:24 +0100

    #06 Maximum balance

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,5 +1,7 @@
 class Oystercard
 
+  DEFAULT_MAXIMUM = 90 
+
   attr_reader :balance
 
   def initialize(balance=0)
@@ -7,7 +9,15 @@ class Oystercard
   end
 
   def top_up(monies)
+    @monies = monies 
+    fail "Topping up this amount will take you over the £#{DEFAULT_MAXIMUM} maximum" if over_maximum?
     @balance += monies
+  end
+
+private 
+
+  def over_maximum?
+    @balance + @monies > DEFAULT_MAXIMUM
   end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,9 +4,11 @@ describe Oystercard do
 
 it {is_expected.to respond_to(:top_up).with(1).argument}
 
+#let(:monies) {double :monies}
+
 describe "#top_up" do
   it "should add money to oyster card" do 
-  expect(subject.top_up(100)).to eq(subject.balance)
+  expect(subject.top_up(1)).to eq(subject.balance)
   end 
 end
 
@@ -16,5 +18,13 @@ describe "#balance" do
   end
 end
 
+describe "#top_up error" do
+  it "raise an error if top up takes over maximum" do
+  maximum_balance = Oystercard::DEFAULT_MAXIMUM
+  message = "Topping up this amount will take you over the £#{maximum_balance} maximum"
+  subject.top_up maximum_balance
+  expect{subject.top_up(1)}.to raise_error(message)
+  end
+end
 
 end

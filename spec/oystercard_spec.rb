bcc76b48a13b1027c9740042f5927b56b2e3640a
Osneil Drakes <osneil-drakes@hotmail.com>
2021-09-05 11:32:21 +0100

    #13 create a station class

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -101,3 +101,19 @@ end
   end
 
 end
+
+require 'station'
+
+describe Station do 
+
+  subject {described_class.new( "Old Street", 1)}
+
+  it 'knows its name' do 
+  expect(subject.name).to eq ("Old Street")
+  end
+
+  it 'knows its zone' do 
+  expect(subject.zone).to eq 1
+  end
+
+end
